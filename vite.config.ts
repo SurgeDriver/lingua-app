52287750d8c7668f1a4606e1de8d29c6a188fa3a
SD <99142325+SurgeDriver@users.noreply.github.com>
2025-08-19 12:17:35 +0000

    Update vite.config.ts 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  //   server: {
-  //   open: true,
-  // },
-  optimizeDeps: {
-    include: ['react-router-dom'],
-  },
-  // fs: {
-  //     allow: ['.'],
-  //   },
-  server: {
-    open: true,
-    proxy: {
-      '/auth0': {
-        target: 'https://dev-kp3nnrzxlqgbwsw1.us.auth0.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/auth0/, ''),
-        secure: false,
-        headers: {
-          'Access-Control-Allow-Origin': 'http://localhost:5173',
-          'Access-Control-Allow-Credentials': 'true'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    //   server: {
+    //   open: true,
+    // },
+    optimizeDeps: {
+      include: ['react-router-dom'],
+    },
+    // fs: {
+    //     allow: ['.'],
+    //   },
+    server: {
+      open: true,
+      proxy: {
+        '/auth0': {
+          target: `https://${env.VITE_AUTH0_DOMAIN}`,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/auth0/, ''),
+          secure: false,
+          headers: {
+            'Access-Control-Allow-Origin': 'http://localhost:5173',
+            'Access-Control-Allow-Credentials': 'true'
+          }
         }
       }
     }
