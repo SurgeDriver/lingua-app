3808ed9e7fb5b88712d1c68a3acc9bf70a912dfd
SD <99142325+SurgeDriver@users.noreply.github.com>
2025-08-19 12:24:11 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,17 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, '.', '')
 
   return {
     plugins: [react()],
-    //   server: {
-    //   open: true,
-    // },
-    optimizeDeps: {
-      include: ['react-router-dom'],
-    },
-    // fs: {
-    //     allow: ['.'],
-    //   },
     server: {
-      open: true,
       proxy: {
         '/auth0': {
           target: `https://${env.VITE_AUTH0_DOMAIN}`,
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/auth0/, ''),
+          rewrite: path => path.replace(/^\/auth0/, ''),
           secure: false,
           headers: {
             'Access-Control-Allow-Origin': 'http://localhost:5173',
