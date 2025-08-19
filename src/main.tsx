af0b649304510dcfe27fb1607ff8bc8df2ee8e01
SD <99142325+SurgeDriver@users.noreply.github.com>
2025-08-19 11:00:48 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,52 @@
-import { StrictMode } from "react";
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './store/store';
 import { StyledEngineProvider } from '@mui/material';
+import { getCookie } from './user/utils/cookies';
 import App from './App';
 import './user/variables.scss';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <StyledEngineProvider injectFirst>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    </StyledEngineProvider>
-  </StrictMode>
-);
+const fetchManagementToken = async () => {
+  try {
+    const response = await fetch('auth0/oauth/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        client_id: import.meta.env.VITE_AUTH0_CLIENT_ID,
+        client_secret: import.meta.env.VITE_AUTH0_CLIENT_SECRET,
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+        grant_type: 'client_credentials',
+      }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(`HTTP error! status: ${response.status}, message: ${JSON.stringify(errorData)}`);
+    }
+
+    const data = await response.json();
+    sessionStorage.setItem('management_token', data.access_token);
+    console.log('Management token:', getCookie('management_token'));
+    return true;
+  } catch (error) {
+    console.error('Ошибка получения токена:', error);
+    return false;
+  }
+};
+
+fetchManagementToken().then((success) => {
+  if (success) {
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+      <StrictMode>
+        <StyledEngineProvider injectFirst>
+          <Provider store={store}>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </Provider>
+        </StyledEngineProvider>
+      </StrictMode>
+    );
+  } else {
+    console.error('Не удалось загрузить приложение: management_token не получен');
+  }
+})
